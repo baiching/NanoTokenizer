43b304a73a903b5e47aade4daed93a6935260853
baiching <bachmarma@gmail.com>
2025-08-02 10:54:12 +0200

    Splitter based on sub tokens such as dots and underscores

---
+++ src/main/java/com/baiching/splitter/SubTokenSplitter.java
@@ -0,0 +1,15 @@
+package com.baiching.splitter;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SubTokenSplitter {
+    public List<String> split(String path){
+        //path = path.split("/");
+
+        // "[._-]"
+        List<String> tokens = Arrays.asList(path.split("[./_\\\\-]"));
+
+        return tokens;
+    }
+}
