671ba64657592734dd6194ad7617bf355dc35a32
baiching <bachmarma@gmail.com>
2025-08-05 01:02:10 +0200

    creates a set of stopwords from string list of stopwords

--- src/main/java/com/baiching/filters/StopWordFilter.java
+++ src/main/java/com/baiching/filters/StopWordFilter.java
@@ -5,10 +5,24 @@ import java.util.*;
 public class StopWordFilter {
     /*
     * creates a set of stopwords from the array
+    *
     * @param stopwords stop word array
+    * @return a Set containing stopWords
      */
 
     public Set<String> createStopWordSet(String[] stopwords) {
         return new HashSet<>(Arrays.asList(Objects.requireNonNull(stopwords, "stopWords")));
+    }
+
+    /*
+     * creates a set of stopwords from the String List
+     *
+     * @param stopwords stop word List
+     * @return a Set containing stopWords
+     */
+
+    public Set<String> createStopWordSet(List<String> stopWords) {
+        Objects.requireNonNull(stopWords, "stopWords");
+        return new HashSet<>(stopWords);
     }
 }
