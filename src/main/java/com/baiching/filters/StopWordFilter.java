121a00bc56217ccf5defaa9e7e8542b31d7b21fb
baiching <bachmarma@gmail.com>
2025-08-05 18:24:46 +0200

    added filter to remove stopWords from the list

--- src/main/java/com/baiching/filters/StopWordFilter.java
+++ src/main/java/com/baiching/filters/StopWordFilter.java
@@ -3,25 +3,45 @@ package com.baiching.filters;
 import java.util.*;
 
 public class StopWordFilter {
-    /*
+    private final Set<String> stopWords;
+
+    public StopWordFilter(Set<String> stopWords) {
+        this.stopWords = Objects.requireNonNull(stopWords, "StopWords cannot be null");
+    }
+    /**
+     * Filters out stopwords from a list of tokens.
+     * @param tokens Input tokens (e.g., ["the", "quick", "brown", "fox"])
+     * @return Filtered tokens (e.g., ["quick", "brown", "fox"])
+     */
+    public List<String> filterStopWords(List<String> tokens) {
+        List<String> filteredTokens = new ArrayList<>();
+        for (String token: tokens) {
+            if (!stopWords.contains(token.toLowerCase())) {
+                filteredTokens.add(token);
+            }
+        }
+
+        return filteredTokens;
+    }
+
+    /**
     * creates a set of stopwords from the array
     *
-    * @param stopwords stop word array
+    * @param stopWords stop word array
     * @return a Set containing stopWords
      */
-
-    public Set<String> createStopWordSet(String[] stopwords) {
+    public static Set<String> createStopWordSet(String[] stopwords) {
         return new HashSet<>(Arrays.asList(Objects.requireNonNull(stopwords, "stopWords")));
     }
 
-    /*
+    /**
      * creates a set of stopwords from the String List
      *
-     * @param stopwords stop word List
+     * @param stopWords stop word List
      * @return a Set containing stopWords
      */
 
-    public Set<String> createStopWordSet(List<String> stopWords) {
+    public static Set<String> createStopWordSet(List<String> stopWords) {
         Objects.requireNonNull(stopWords, "stopWords");
         return new HashSet<>(stopWords);
     }
